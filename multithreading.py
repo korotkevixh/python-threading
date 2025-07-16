50a25213ddf7e9b5e0b8503ef07116f9f9b0de5d
korotkevixh <korotkevixh@gmail.com>
2025-07-16 18:20:08 +0300

    Modify with queue

--- multithreading.py
+++ multithreading.py
@@ -1,22 +1,40 @@
 import threading    # для создания и управления потоками
 import requests     # для выполнения HTTP-запросов
 import time         # для замера времени выполнения
+import queue
 
 
 urls = ["https://deelay.me/2000/https://example.com"] * 5
+print_lock = threading.Lock() # defend for print
+task_queue = queue.Queue()
 
-def download(url):
-    print(f"Starting {url}")
-    response = requests.get(url)
-    print(f"{url} done with status {response.status_code}")
+def worker():
+    while True:
+        url = task_queue.get()
+        if url is None:
+            break
+        with print_lock:
+            print(f"[{threading.current_thread().name}] Starting {url}")
+        response = requests.get(url)
+        with print_lock:
+            print(f"[{threading.current_thread().name}] {url} done with status {response.status_code}")
+        task_queue.task_done()
 
 start = time.time()
 threads = []
 
-for url in urls:
-    t = threading.Thread(target=download, args=(url,))
+for _ in range(3):
+    t = threading.Thread(target=worker)
     t.start()
     threads.append(t)
+
+for url in urls:
+    task_queue.put(url)
+
+task_queue.join()
+
+for _ in threads:
+    task_queue.put(None)
 
 for t in threads: 
     t.join()
